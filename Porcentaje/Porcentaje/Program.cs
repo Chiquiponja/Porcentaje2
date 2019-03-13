a099c006bbbb830e837287cc7dfcb5bcf56dc2cb
Chiquiponja <46056373+Chiquiponja@users.noreply.github.com>
2019-03-13 12:26:17 -0300

    Add files via upload

---
+++ Porcentaje/Porcentaje/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReglaDeIntereses
+{
+    class EjercicioComprobante
+    {
+       
+
+        static void Main(string[] args)
+        {
+
+            Comprobante.Comprobante comp1 = new Comprobante.Comprobante();
+            comp1.TipoDeComprobante = ("Factura A");
+            comp1.DatosClientes = ("Lucia");
+            comp1.FechaDePago =  DateTime.Now;
+            comp1.FechaEm = new DateTime(2019, 2, 5);
+            comp1.FechaVen = new DateTime(2019, 2, 25);
+            comp1.Importe = 2000;
+
+            //var recargoService = new RegladeRecargoService();
+            //var recargoCobrar = recargoService.
+
+
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Porcentaje/Porcentaje/RegladeRecargoService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Porcentaje
+{
+    public class RegladeRecargoService
+    {
+        public class Calcular
+        {
+            
+            decimal CalcularRecargo(Comprobante.Comprobante)
+            {
+                decimal Recargo = 0;
+
+                
+
+
+
+
+
+             return Recargo;
+                
+            }
+           
+        }
+        
+    }
+}
